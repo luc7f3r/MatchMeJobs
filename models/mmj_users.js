8999cb685aa986081a8eb5820071ea27bed0a8f1
luc7f3r <79524030+luc7f3r@users.noreply.github.com>
2024-09-19 13:32:19 +0530

    Update mmj_users.js

--- models/mmj_users.js
+++ models/mmj_users.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
-    full_name : {type : String, required: true},
-    email : {type : String, required:true},
-    contact_number : {type : String, required:true},
-    job_role : {type: String, required: true},
-    level : {type : String, required: true},
-},{timestamps : true});
+// const schema = new mongoose.Schema({
+//     full_name : {type : String, required: true},
+//     email : {type : String, required:true},
+//     contact_number : {type : String, required:true},
+//     job_role : {type: String, required: true},
+//     level : {type : String, required: true},
+// },{timestamps : true});
 
-const mmj_user = mongoose.model("mmj_user", schema);
+// const mmj_user = mongoose.model("mmj_user", schema);
 
-module.exports = mmj_user;
+// module.exports = mmj_user;
